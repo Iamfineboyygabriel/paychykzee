cb2c04fd627d870a7727b7de1b9c39a1291420a6
Akintola Babatunde Rafiu <tundegabriel999@gmail.com>
2024-06-01 09:33:59 +0100

    working on responsiveness

--- src/components/contents/footer/Footer.tsx
+++ src/components/contents/footer/Footer.tsx
@@ -36,22 +36,22 @@ export const Footer = () => {
           </div>
         </div>
         <hr className="border-1 mt-[2em] border-line" />
-        <div className="flex items-center justify-between gap-4 rounded-full bg-inherit px-3 py-1.5 text-textp sm:flex-col lg:flex-row lg:border-line lg:bg-purpleblack">
+        <div className="mt-[2em] flex items-center justify-between gap-2 rounded-full border border-none bg-inherit px-3 py-1.5 text-textp sm:flex-col lg:flex-row lg:border-line lg:bg-purpleblack">
           <div className="flex gap-3">
             <div>
-              <img src={facebook} alt="facebook-logo" />
+              <img src={facebook} alt="facebookk-logo" />
             </div>
             <div>
               <img src={tweeter} alt="tweeter-logo" />
             </div>
             <div>
-              <img src={lnkdln} alt="linkedin-logo" />
+              <img src={lnkdln} alt="tweeter-logo" />
             </div>
           </div>
           <div>
             <p className="font-br-light">PayChykzee All Rights Reserved</p>
           </div>
-          <div>
+          <div className="">
             <p className="font-br-light">Privacy Policy | Terms of service</p>
           </div>
         </div>
